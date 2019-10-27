08aa7958524445abba73a095c864c19e08364393
Abhishek5101 <miabhishekkulkarni@gmail.com>
2019-10-27 14:12:54 -0700

    histogram, word frequency, and unique words modules added

---
+++ analyze.py
@@ -0,0 +1,30 @@
+
+def calculate_histogram(source_text):
+	with open(source_text) as book:
+		data = book.readlines()
+	
+	words = [word.strip() for word in data]
+	text_to_use = ' '.join(words)
+	
+	words_list = text_to_use.split(' ')
+	
+	text_dict = {}
+	for word in words_list:
+		text_dict[word] = 0
+	for word in words_list:
+		text_dict[word] += 1
+	return text_dict
+
+
+def unique_words(histogram):
+	return len(histogram)
+
+
+def frequency(word, histogram):
+	if histogram[word]:
+		return histogram[word]
+	else:
+		return f'No Such Word'
+
+
+print(calculate_histogram('opticks.txt'))
--- rearrange.py
+++ rearrange.py
@@ -2,15 +2,19 @@ import random
 
 
 def rearrange(*words):
+	random_indexes_chosen = []
 	word_list = [i for i in words]
 	ans = ""
 	c = 0
 	while c != len(word_list):
-		random_word = random.randint(0, len(word_list)-1)
-		if word_list[random_word] not in ans:
-			ans += " " + word_list[random_word]
+		random_index = random.randint(0, len(word_list)-1)
+		if word_list[random_index] not in ans:
+			ans += " " + word_list[random_index]
 			c += 1
 	return print(ans)
 
 
-rearrange("hey", "now", "brown", "cow")
+sentence = "flying bats and scary cats"
+words = sentence.split()
+
+rearrange(*words)
