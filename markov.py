29559630c9fa3a418900ac85af1a9eb592ccc82f
Abhishek5101 <miabhishekkulkarni@gmail.com>
2019-11-15 13:50:21 -0800

    markov chain with sample method added

--- markov.py
+++ markov.py
@@ -1,7 +1,7 @@
 from dictogram import Dictogram
 from random import choice
 
-sample_text = "one fish two fish three fish".split(' ')
+sample_text = " one fish two fish blue fish".split(' ')
 
 markov = {}
 
@@ -12,11 +12,10 @@ for index in range(len(sample_text) - 1):
 	markov[sample_text[index]].add_count(sample_text[index + 1])
 
 # print(markov['fish'].sample())
-print(markov)
 sentence = " "
 word = choice(list(markov.keys()))
 sentence += " " + word
-for i in range(8):
-	word = choice(list(markov[word].keys()))
+for i in range(4):
+	word = markov[word].sample()
 	sentence += " " + word
 print(sentence)
